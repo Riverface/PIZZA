9b50fa59e694c415cb9072c7252b8de11acdbf48
Akigawa <sonoskeakigawa@gmail.com>
2019-05-18 13:40:52 -0700

    Soon

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,62 @@
 $(document).ready(function() {
-  init();
-  $("#ashdust").hide();
-  $("#ashdustvideo").on('ended',function(){
-    location.reload();
-  });
+  Init();
+
+
 });
 
-function init(){
+function Init(){
   var parlor = new Parlor();
   var user = new Cart();
-  parlor.sizes = [new Size("small", 6.25),new Size("medium", 8.00),new Size("Large",10.00)];
-  pepperoni = new Topping("Pepperoni", 1.00);
-  sausage = new Topping("sausage", 1.00);
-  console.log(parlor.sizes);
-  parlor.toppings.push( new Topping("Dust", 0));
-  parlor.toppings.push( new Topping("Ash", 0));
-  parlor.toppings.push(sausage);
-  parlor.toppings.push(pepperoni);
-  $("#sizes").append(parlor.listsizes());
-  $("#toppings").append(parlor.ToppingWriter());
-  console.log(parlor);
+  user.currpos=0;
+  AddProps(parlor);
+  HideBotnik();
+  InitialPrint(parlor);
+  AddHooks(parlor,user);
+}
+function AddProps(parl){
 
-  console.log(user);
+  parl.sizes = [new Size("small", 6.25),new Size("medium", 8.00),new Size("Large",10.00)];
+  parl.toppings = [
+    new Topping("Pepperoni", 1.00),
+    new Topping("sausage", 1.00),
+    new Topping("Dust", 0),
+    new Topping("Ash", 0)
+  ];
+}
+
+function AddHooks(parl,cart){
   $("#addbutton").click(function(){
     $("#subtotal").empty();
     peetz = new Pizza();
     $("#toppings").val().forEach(function(top){
-      peetz.toppings.push(parlor.toppings[top]);
+      peetz.toppings.push(parl.toppings[top]);
     });
-
-    console.log(parlor.sizes[$("#sizes").val()]);
-    peetz.size = parlor.sizes[$("#sizes").val()];
-    console.log(peetz);
-    console.log($("#sizes").val());
-    console.log(peetz.size);
+    peetz.size = parl.sizes[$("#sizes").val()];
     CheckBotnik(peetz);
-    user.pizzas.push(peetz);
-
-    ReceiptGen(user);
-
-    console.log(parlor);
+    cart.pizzas.push(peetz);
+    CartGen(cart);
   });
+
+  $("#buybutton").click(function(){
+    BuyPizza(cart);
+  });
+  $("#histfw").click(function(){
+    MoveHist(1,cart.currpos,cart);
+  });
+  $("#histbk").click(function(){
+    MoveHist(-1,cart.currpos,cart);
+  });
+}
+
+function HideBotnik(){
+  $("#ashdust").hide();
+  $("#ashdustvideo").on('ended',function(){
+    location.reload();
+  });
+}
+function InitialPrint(parl){
+  $("#sizes").append(parl.listsizes());
+  $("#toppings").append(parl.ToppingWriter());
 }
 function CheckBotnik(pizz){
   iterate = 0;
@@ -59,19 +75,19 @@ function CheckBotnik(pizz){
 
     if(iterate == 2){
       $("#createpizza").hide();
-      $("#subtotal").hide();
       $("#buypizza").hide();
       $("#ashdust").show();
       $("#pizzasign").hide();
+      $("#subtcon").hide();
       $("#ashdustvideo")[0].play();
     }
   });
 }
-
-
 function Cart(pizzas){
   this.pizzas = [];
   this.price = countsubtotal(this);
+  this.history = []
+  this.currpos;
 }
 function countsubtotal(shoppingcart){
   shoppingcart.pizzas.forEach(function(peetza){
@@ -106,17 +122,17 @@ function Pizza(size){
 
 Parlor.prototype.ToppingWriter = function(){
   writer = "";
-  console.log(this.toppings);
+
   for(var currtop = 0; this.toppings.length > currtop; currtop++){
     writer+="<option value='" + currtop + "'>" + this.toppings[currtop].name + "</option>";
-    console.log(this.toppings[currtop]);
+
   }
   return writer;
 }
-function ReceiptGen(cart){
+function CartGen(cart){
   $("#subtotal").empty();
   writer = null;
-  console.log(writer);
+
   writer = "<div class='propertyLabel'>Receipt</div>";
   writer += "<div class='property'>";
   pizziterate = 0;
@@ -125,51 +141,38 @@ function ReceiptGen(cart){
     writer += thispizza.size.name;
     if(thispizza.toppings.length == 0){
 
-     writer += thispizza.size.price + " pizza";
-   }
+      writer += thispizza.size.price + " pizza";
+    }
     if(thispizza.toppings.length > 0){
-       writer+= "with:<br>";
+      writer+= "with:<br>";
     }
 
     thispizza.toppings.forEach(function(top){
 
-      console.log(top);
+
       writer+= top.name;
       writer+= top.price.toFixed(2);
       writer+= "<br>";
 
     });
     writer+= "<br>"+thispizza.price();
-    //writer += pizza.price();
-    //writer+= "</div>";
-    writer += "<input type='button' value='x' id='closepizza"+ pizziterate+"'> </input><hr>";
+
+    writer+= "</div>";
+
 
     console.log($("#pizza"+pizziterate));
     console.log(writer);
     $("#subtotal").html(writer);
-
+    $("#subtotal").append("</div>");
     pizziterate++;
-    });
 
-
-    //AddCloseButton(pizziterate,cart);
-  writer = "</div>";
-  $("#subtotal").append(writer);
-
-  for(pizziterate=0;pizziterate<cart.pizzas.length;pizziterate++){
-
-  $("#closepizza"+pizziterate).click(function(){
-    console.log(pizziterate + "Removed");
-    cart.pizzas.splice(pizziterate-1,1);
-
-    ReceiptGen(cart);
-    console.log(cart);
   });
 
 
-}
+  //AddCloseButton(pizziterate,cart);
 
-
+  AddCloseButton(cart);
+  return writer;
 }
 
 Parlor.prototype.listsizes = function(){
@@ -184,15 +187,31 @@ Parlor.prototype.listsizes = function(){
     sizewriter += "'>";
     sizewriter += this.sizes[currsize].name;
     sizewriter += "</option>";
-    console.log(sizewriter);
+
   }
   return sizewriter;
 }
-function AddCloseButton(idofpizza,cart){
 
+function AddCloseButton(cart){
+  secondwriter = "";
 
+  for(var pizziterate=0;pizziterate < cart.pizzas.length;pizziterate++){
+        secondwriter += "<input type='button' value='x' id='closepizza"+ (pizziterate) +"'> </input><hr>";
+        console.log(secondwriter);
+            console.log(pizziterate);
+            $("#pizza"+pizziterate).append(secondwriter);
+            secondwriter="";
+  }
 
+  secondwriter = "";
+  for(pizziterate=0;pizziterate < cart.pizzas.length;pizziterate++){
+            $("#closepizza"+pizziterate).click(function() {
+              CartGen(cart);
+              cart.pizzas.splice(pizziterate-1,1);
+            });
+  }
 }
+
 Pizza.prototype.price = function(){
   theprice = this.size.price;
   this.toppings.forEach(function(topp){
@@ -207,5 +226,138 @@ Cart.prototype.total = function(){
   });
 }
 Pizza.prototype.size = function(){
+}
+function BuyPizza(cart){
+  cart.history.push(CartGen(cart));
+  console.log(cart.history);
+  TotalPrint(cart);
+}
 
+function TotalPrint(cart){
+  $("#subtotal").empty();
+  writer = null;
+
+  writer = "<div class='propertyLabel'>Receipt</div>";
+  writer += "<div class='property'>";
+  pizziterate = 0;
+  cart.pizzas.forEach(function(thispizza){
+    writer += "<br><div id='pizza"+ pizziterate +"'>";
+    writer += thispizza.size.name;
+    if(thispizza.toppings.length == 0){
+
+      writer += thispizza.size.price + " pizza";
+    }
+    if(thispizza.toppings.length > 0){
+      writer+= "with:<br>";
+    }
+
+    thispizza.toppings.forEach(function(top){
+
+
+      writer+= top.name;
+      writer+= top.price.toFixed(2);
+      writer+= "<br>";
+
+    });
+    writer+= "<br>"+thispizza.price();
+
+    writer+= "</div>";
+
+
+    console.log($("#pizza"+pizziterate));
+    console.log(writer);
+    $("#subtotal").html(writer);
+    $("#subtotal").append("</div>");
+    pizziterate++;
+
+  });
+
+
+  //AddCloseButton(pizziterate,cart);
+
+  AddCloseButton(cart);
+  return writer;
+  thirdwriter = "";
+
+
+  cart.history.forEach(function(histentry){
+    thirdwriter  += "<div id='history"+ cart.currpos+"'>";
+
+    thirdwriter += histentry;
+    thirdwriter  +="</div>";
+  });
+
+  $("#history").html(thirdwriter);
+  $("#subtotal").html("");
+  cart.order = [];
+  $("#history"+ cart.currpos).hide();
+  if(currpos == 0){
+    $("#history"+ cart.currpos).show();
+  }
+  cart.currpos++;
+}
+
+function MoveHist(dir,currpos,cart){
+console.log(currpos);
+if(cart.history.length == 1){
+currpos = 0;
+}
+if(currpos >  0){
+if(currpos<cart.history.length){
+$("#history"+currpos).hide();
+$("#history"+currpos).show();
+currpos+=dir;
+}
+}
+else{
+
+  $("#history"+currpos).show();
+}
+
+
+
+}
+ReceiptGen(cart){
+
+  $("#receipts").empty();
+  writer = null;
+
+  writer = "<div class='propertyLabel'>Receipt</div>";
+  writer += "<div class='property'>";
+  pizziterate = 0;
+  cart.pizzas.forEach(function(thispizza){
+
+    writer += thispizza.size.name;
+    if(thispizza.toppings.length == 0){
+
+      writer += thispizza.size.price + " pizza";
+    }
+    if(thispizza.toppings.length > 0){
+      writer+= "with:<br>";
+    }
+
+    thispizza.toppings.forEach(function(top){
+
+
+      writer+= top.name;
+      writer+= top.price.toFixed(2);
+      writer+= "<br>";
+
+    });
+    writer+= "<br>"+thispizza.price();
+
+    writer+= "</div>";
+
+
+    console.log($("#pizza"+pizziterate));
+    console.log(writer);
+    $("#subtotal").html(writer);
+    $("#subtotal").append("</div>");
+    pizziterate++;
+
+  });
+
+
+  //AddCloseButton(pizziterate,cart);
+  return writer;
 }
